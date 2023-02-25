feab5c029f424fa30499fde41cb7bcb495567904
erhansennx <erhanfatih33@gmail.com>
2023-02-26 01:16:28 +0300

    Some of the designs are finished. Stopwatch implemented.

--- app/src/main/java/com/erhansen/pomodoro/MainActivity.kt
+++ app/src/main/java/com/erhansen/pomodoro/MainActivity.kt
@@ -2,10 +2,68 @@ package com.erhansen.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.SystemClock
+import androidx.core.content.ContextCompat
+import com.erhansen.pomodoro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val countDownTime = 60
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        with(activityMainBinding) {
+
+
+
+            toggleGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
+
+                if (isChecked) {
+                    when (checkedId) {
+                        R.id.shortBreakButton -> {
+                            linearLayout.setBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.light_blue))
+                        }
+                        R.id.pomodoroButton -> {
+                            linearLayout.setBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.red))
+                            chronometer.base = SystemClock.elapsedRealtime() + (countDownTime * 1000)
+                            chronometer.format = "mm:ss"
+                            val endTime = SystemClock.elapsedRealtime() + (countDownTime * 1000)
+                            val countDownTimer = object : CountDownTimer(605000, 1000) {
+                                override fun onTick(millisUntilFinished: Long) {
+                                    val seconds = (millisUntilFinished / 1000).toInt()
+                                    val minutes = seconds / 60
+                                    val remainderSeconds = seconds % 60
+                                    if (minutes < 10 && remainderSeconds < 10) chronometer.text = "0$minutes:0$remainderSeconds"
+                                    else if (minutes < 10 && remainderSeconds > 10) chronometer.text = "0$minutes:$remainderSeconds"
+                                    else if (minutes > 10 && remainderSeconds < 10) chronometer.text = "$minutes:0$remainderSeconds"
+                                    else chronometer.text = "$minutes:$remainderSeconds"
+
+                                }
+
+                                override fun onFinish() {
+                                    chronometer.text = "00:00"
+                                }
+                            }.start()
+                        }
+                        R.id.longBreakButton -> {
+                            linearLayout.setBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.dark_blue))
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+
     }
+
+
+
+
 }
