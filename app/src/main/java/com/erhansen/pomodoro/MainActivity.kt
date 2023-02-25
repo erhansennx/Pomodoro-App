bd1557c11ac01610841c04d0fc19835eb69bdf82
erhansennx <erhanfatih33@gmail.com>
2023-02-26 01:34:12 +0300

    created function for timers

--- app/src/main/java/com/erhansen/pomodoro/MainActivity.kt
+++ app/src/main/java/com/erhansen/pomodoro/MainActivity.kt
@@ -20,47 +20,66 @@ class MainActivity : AppCompatActivity() {
         with(activityMainBinding) {
 
 
-
             toggleGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
 
                 if (isChecked) {
                     when (checkedId) {
                         R.id.shortBreakButton -> {
                             linearLayout.setBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.light_blue))
+                            chronometer.text = "05:00"
+                            startButton.setOnClickListener {
+                                startTimer(300000)
+                            }
                         }
                         R.id.pomodoroButton -> {
                             linearLayout.setBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.red))
-                            chronometer.base = SystemClock.elapsedRealtime() + (countDownTime * 1000)
-                            chronometer.format = "mm:ss"
-                            val endTime = SystemClock.elapsedRealtime() + (countDownTime * 1000)
-                            val countDownTimer = object : CountDownTimer(605000, 1000) {
-                                override fun onTick(millisUntilFinished: Long) {
-                                    val seconds = (millisUntilFinished / 1000).toInt()
-                                    val minutes = seconds / 60
-                                    val remainderSeconds = seconds % 60
-                                    if (minutes < 10 && remainderSeconds < 10) chronometer.text = "0$minutes:0$remainderSeconds"
-                                    else if (minutes < 10 && remainderSeconds > 10) chronometer.text = "0$minutes:$remainderSeconds"
-                                    else if (minutes > 10 && remainderSeconds < 10) chronometer.text = "$minutes:0$remainderSeconds"
-                                    else chronometer.text = "$minutes:$remainderSeconds"
-
-                                }
-
-                                override fun onFinish() {
-                                    chronometer.text = "00:00"
-                                }
-                            }.start()
+                            chronometer.text = "25:00"
+                            startButton.setOnClickListener {
+                                startTimer(1500000)
+                            }
                         }
                         R.id.longBreakButton -> {
                             linearLayout.setBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.dark_blue))
+                            chronometer.text = "15:00"
+                            startButton.setOnClickListener {
+                                startTimer(900000)
+                            }
                         }
                     }
                 }
 
             }
 
+
+
         }
 
 
+    }
+
+
+    private fun startTimer(time: Long) {
+        with(activityMainBinding) {
+            chronometer.base = SystemClock.elapsedRealtime() + (countDownTime * 1000)
+            chronometer.format = "mm:ss"
+            val endTime = SystemClock.elapsedRealtime() + (countDownTime * 1000)
+            val countDownTimer = object : CountDownTimer(time, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    val seconds = (millisUntilFinished / 1000).toInt()
+                    val minutes = seconds / 60
+                    val remainderSeconds = seconds % 60
+                    if (minutes < 10 && remainderSeconds < 10) chronometer.text = "0$minutes:0$remainderSeconds"
+                    else if (minutes < 10 && remainderSeconds > 10) chronometer.text = "0$minutes:$remainderSeconds"
+                    else if (minutes > 10 && remainderSeconds < 10) chronometer.text = "$minutes:0$remainderSeconds"
+                    else chronometer.text = "$minutes:$remainderSeconds"
+
+                }
+
+                override fun onFinish() {
+                    chronometer.text = "00:00"
+                }
+            }.start()
+        }
     }
 
 
